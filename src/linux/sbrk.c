4bf8c4281cd9f32b3e141aae324aeddddfb44ed2
Alon Zakai <alonzakai@gmail.com>
2015-05-22 14:09:46 -0700

    disable sbrk, emscripten controls it itself

--- src/linux/sbrk.c
+++ src/linux/sbrk.c
@@ -2,8 +2,11 @@
 #include <errno.h>
 #include "syscall.h"
 
+#if !__EMSCRIPTEN__ /* Emscripten controls sbrk itself */
 void *sbrk(intptr_t inc)
 {
 	if (inc) return (void *)__syscall_ret(-ENOMEM);
 	return (void *)__syscall(SYS_brk, 0);
 }
+#endif
+
